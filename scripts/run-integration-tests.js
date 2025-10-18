da7309e877cc980f55f15136521c69010a4b1aa3
hubKea <kea.maripane@icloud.com>
2025-10-18 15:01:21 +0200

    Format integration test runner

--- scripts/run-integration-tests.js
+++ scripts/run-integration-tests.js
@@ -6,10 +6,14 @@ const path = require('path');
 const jestExecutable = path.resolve(__dirname, '..', 'node_modules', 'jest', 'bin', 'jest.js');
 const additionalArgs = process.argv.slice(2);
 
-const child = spawn(process.execPath, ['--experimental-vm-modules', jestExecutable, 'tests/integration', ...additionalArgs], {
-  stdio: 'inherit',
-  env: process.env,
-});
+const child = spawn(
+  process.execPath,
+  ['--experimental-vm-modules', jestExecutable, 'tests/integration', ...additionalArgs],
+  {
+    stdio: 'inherit',
+    env: process.env,
+  }
+);
 
 child.on('exit', (code, signal) => {
   if (signal) {

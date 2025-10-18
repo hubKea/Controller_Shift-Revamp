fc5440dc4af99b9ff61616b7a7039e230393116c
hubKea <kea.maripane@icloud.com>
2025-10-18 14:59:23 +0200

    Fix integration test script with Firebase emulator

---
+++ scripts/run-integration-tests.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const path = require('path');
+
+const jestExecutable = path.resolve(__dirname, '..', 'node_modules', 'jest', 'bin', 'jest.js');
+const additionalArgs = process.argv.slice(2);
+
+const child = spawn(process.execPath, ['--experimental-vm-modules', jestExecutable, 'tests/integration', ...additionalArgs], {
+  stdio: 'inherit',
+  env: process.env,
+});
+
+child.on('exit', (code, signal) => {
+  if (signal) {
+    process.kill(process.pid, signal);
+  } else {
+    process.exit(code ?? 1);
+  }
+});
